2b2a49cff35cfeeca346373d689fb3d5a2c50c42
Ashika <ashikahm@gmail.com>
2022-10-12 16:58:54 +0530

    added search handling

--- PersonInfo/PersonInfo/View/ViewInformation.xaml.cs
+++ PersonInfo/PersonInfo/View/ViewInformation.xaml.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using PersonInfo.ViewModel;
 
 namespace PersonInfo.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ViewInformation : ContentPage
     {
+        methodCall obj = new methodCall();
         public ViewInformation()
         {
             InitializeComponent();
+            MyListView.ItemsSource = obj.GetInfo;
         }
 
         private async void ImageButton_Clicked(object sender, EventArgs e)
@@ -22,6 +25,13 @@ namespace PersonInfo.View
             var ib = sender as ImageButton;
             var url = ib.CommandParameter.ToString();
             await Browser.OpenAsync(url);
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            MyListView.ItemsSource = obj.GetInfo.Where(a => a.Name.StartsWith(e.NewTextValue));
+
         }
     }
 }
